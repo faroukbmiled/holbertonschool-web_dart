47673733a8cad817be9e7a714168375f5f4f4fba
faroukbmiled <fcfaroukfc@gmail.com>
2023-07-19 02:08:51 +0100

    fix?

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,22 @@
 void outer(String name, String id) {
-  print("Hello Agent B.$name your id is $id");
+  String[] nameParts = inner(name);
+  String firstName = nameParts[0];
+  String lastName = nameParts[1];
+
+  System.out.println("Hello Agent B." + firstName + " your id is " + id);
 }
 
-String inner() {
-  return "";
+String[] inner(String name) {
+  String[] nameParts = name.split(" ");
+  String[] result = new String[2];
+
+  if (nameParts.length > 0) {
+    result[0] = nameParts[0];
+    result[1] = nameParts.length > 1 ? nameParts[1] : "";
+  } else {
+    result[0] = "";
+    result[1] = "";
+  }
+
+  return result;
 }
