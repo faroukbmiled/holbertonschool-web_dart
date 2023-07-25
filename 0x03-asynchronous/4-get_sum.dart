6e543ff71f17de1feac546ba1cae73b83ae78c3f
faroukbmiled <fcfaroukfc@gmail.com>
2023-07-25 23:08:15 +0100

    add

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,21 @@
+import "dart:convert";
+
+Future<double> calculateTotal() async {
+  try {
+    final user = await fetchUserData();
+    final id = json.decode(user)["id"];
+    final products = await fetchUserOrders(id);
+
+    double totalPrice = 0.0;
+    final productNames = json.decode(products).cast<String>();
+
+    for (String product in productNames) {
+      final price = await fetchProductPrice(product);
+      totalPrice += json.decode(price) as double;
+    }
+
+    return totalPrice;
+  } catch (e) {
+    return (-1);
+  }
+}
