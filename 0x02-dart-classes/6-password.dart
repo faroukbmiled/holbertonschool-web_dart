e828261888cdcc9292b1ad67460bcd2e6de44d3c
faroukbmiled <fcfaroukfc@gmail.com>
2024-01-28 16:42:05 +0100

    fix?

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,18 +1,16 @@
 class Password {
-  String? _password = "";
-  String? get password => _password;
-  set password(String? password) => _password = password ?? '';
+  String _password = "";
 
-  Password({required String password}) {
+  Password({required String? password}) {
     _password = password ?? '';
   }
 
   bool isValid() {
-    if (_password == null) return false;
-    if (_password!.length >= 8 && _password!.length <= 16) {
-      if (_password!.contains(RegExp(r'[A-Z]')) &&
-          _password!.contains(RegExp(r'[a-z]')) &&
-          _password!.contains(RegExp(r'[0-9]'))) {
+    if (_password == null || _password!.isEmpty) return false;
+    if (_password.length >= 8 && _password.length <= 16) {
+      if (_password.contains(RegExp(r'[A-Z]')) &&
+          _password.contains(RegExp(r'[a-z]')) &&
+          _password.contains(RegExp(r'[0-9]'))) {
         return true;
       }
     }
